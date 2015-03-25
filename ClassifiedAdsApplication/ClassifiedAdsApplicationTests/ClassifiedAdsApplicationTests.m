ba3938b61fc8059e849f3da2fb0ce4e07c116fc1
Hassan Aftab <Hassan_pro43@yahoo.com>
2015-03-25 15:47:30 +0500

    Project Added

---
+++ ClassifiedAdsApplication/ClassifiedAdsApplicationTests/ClassifiedAdsApplicationTests.m
@@ -0,0 +1,34 @@
+//
+//  ClassifiedAdsApplicationTests.m
+//  ClassifiedAdsApplicationTests
+//
+//  Created by Coeus on 25/03/2015.
+//  Copyright (c) 2015 Coeus. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ClassifiedAdsApplicationTests : XCTestCase
+
+@end
+
+@implementation ClassifiedAdsApplicationTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
