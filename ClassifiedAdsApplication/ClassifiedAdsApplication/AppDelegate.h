85bdc284adda867b8b8851839f3424d24efef928
Hassan Aftab <Hassan_pro43@yahoo.com>
2015-03-25 15:29:55 +0500

    Project added

---
+++ ClassifiedAdsApplication/ClassifiedAdsApplication/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ClassifiedAdsApplication
+//
+//  Created by Coeus on 25/03/2015.
+//  Copyright (c) 2015 Coeus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
